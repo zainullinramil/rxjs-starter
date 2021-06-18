ff4aa3975482c72ad401f6aecc8926554462d707
Ramil <rzainullin@tab-trader.com>
2021-06-18 19:29:13 +0300

    fromEvent

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,52 @@
 // tslint:disable: no-console
 
-import { from } from "rxjs";
-import { map } from "rxjs/operators";
+import { from, fromEvent, Observable } from "rxjs";
+import { debounceTime, distinctUntilChanged, filter, map, takeUntil } from "rxjs/operators";
 
-const people: string[] = ["Micheal", "Jim", "Dwight"];
+const input = document.getElementById("input-main");
+const stopBtn = document.getElementById("btn-stop");
 
-from(people)
-	.pipe(map((name) => `${name} says hi!`))
+
+// const serach$ = new Observable<Event>(observer => {
+//
+// 	const onSearch = (event: Event) => {
+// 		checkSubscription();
+// 		observer.next(event);
+// 	};
+//
+// 	const clear = () => {
+// 		stopBtn.removeEventListener("click", onStop);
+// 		input.removeEventListener("input", onSearch);
+// 	};
+//
+// 	const checkSubscription = () => {
+// 		if (observer.closed) {
+// 			clear()
+// 		}
+// 	}
+//
+// 	const onStop = () => {
+// 		checkSubscription();
+// 		console.log('onStop');
+// 		observer.complete();
+// 		clear();
+// 	};
+//
+// 	input.addEventListener("input", onSearch);
+// 	stopBtn.addEventListener("click", onStop);
+//
+// });
+
+const search$: Observable<Event> = fromEvent<Event>(document.getElementById('input-main'), 'input')
+const stop$: Observable<Event> = fromEvent<Event>(document.getElementById('btn-stop'), 'click')
+
+
+const searchSub = search$
+	.pipe(
+		debounceTime(500),
+		map((event) => (event.target as HTMLInputElement).value),
+		filter(value => value.length > 3),
+		distinctUntilChanged(),
+		takeUntil(stop$)
+	)
 	.subscribe((message) => console.log(message));
